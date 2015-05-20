529a0b35315cbf230fecdd97300a796fa6a3560e
Nikolay Bankin <niky.bankin@gmail.com>
2015-05-21 01:30:47 +0300

    Basic structure created. Added bootstrap. Removed angular exaples.

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,25 @@
-var app = angular.module('SocialNetworkApp', []);
+var app = angular.module('SocialNetworkApp', ['ngRoute']);
+
+app.constant('serviceUrl', 'http://softuni-social-network.azurewebsites.net/api');
+
+
+app.config(function($routeProvider){
+    $routeProvider.when('/', {
+        templateUrl: 'templates/main/login.html',
+        controller: 'MainController'
+    });
+
+    $routeProvider.when('/login', {
+        templateUrl: 'templates/main/login.html',
+        controller: 'LoginController'
+    });
+
+    $routeProvider.when('/register', {
+        templateUrl: 'templates/main/register.html',
+        controller: 'RegisterController'
+    });
+
+    $routeProvider.otherwise({
+        redirectTo: '/'
+    });
+});
--- app/js/controllers/HomeController.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Nikolay on 5/19/2015.
- */
-app.controller('HomeController', function() {
-
-});
